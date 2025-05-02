d865c9709ba0ec45578e6b9ab07a757ffcdc32c2
Horia Botezatu <h.a.botezatu@student.tudelft.nl>
2025-05-02 11:58:33 +0200

    Added database insertion and tested it

--- server/app/db/connection.py
+++ server/app/db/connection.py
@@ -1,29 +1,59 @@
 import psycopg2
-from config import DB_CONFIG
+from server.app.db.config import DB_CONFIG
+from server.app.models.NtpMeasurement import NtpMeasurement
 
-conn = psycopg2.connect(**DB_CONFIG)
-cur = conn.cursor()
 
-cur.execute("""
-    INSERT INTO times (
-        client_sent, client_sent_prec,
-        server_recv, server_recv_prec,
-        server_sent, server_sent_prec,
-        client_recv, client_recv_prec
-    )
-    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
-    RETURNING id
-""", (
-    12, 1,  # client_sent
-    1714593335001, 223456789,  # server_recv
-    1714595001, 323456789,  # server_sent
-    1714595001, 423456789   # client_recv
-))
-measurement_id = cur.fetchone()[0]
-print(measurement_id)
+def insert_measurement(measurement : NtpMeasurement) :
 
-conn.commit()
-cur.close()
-conn.close()
+    conn = psycopg2.connect(**DB_CONFIG)
+    cur = conn.cursor()
+
+    cur.execute("""
+        INSERT INTO times (
+            client_sent, client_sent_prec,
+            server_recv, server_recv_prec,
+            server_sent, server_sent_prec,
+            client_recv, client_recv_prec
+        )
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+        RETURNING id
+    """, (
+        measurement.timestamps.client_sent_time.seconds, measurement.timestamps.client_sent_time.fraction,
+        measurement.timestamps.server_recv_time.seconds, measurement.timestamps.server_recv_time.fraction,
+        measurement.timestamps.server_sent_time.seconds, measurement.timestamps.server_sent_time.fraction,
+        measurement.timestamps.client_recv_time.seconds, measurement.timestamps.client_recv_time.fraction
+    ))
+
+    time_id = cur.fetchone()[0]
+    print(time_id)
+
+    cur.execute("""
+        INSERT INTO measurements(
+            ntp_server_ip, ntp_server_name,
+            ntp_version, ntp_server_ref_parent,
+            ref_name, time_id,
+             "offset", delay,
+            stratum, precision,
+            reachability, 
+            root_delay,
+            ntp_last_sync_time,
+            root_delay_prec,
+            ntp_last_sync_time_prec
+        )
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+    """, (
+        str(measurement.server_info.ntp_server_ip), measurement.server_info.ntp_server_name,
+        measurement.server_info.ntp_version, str(measurement.server_info.ntp_server_ref_parent_ip),
+        measurement.server_info.ref_name, time_id,
+        measurement.main_details.offset, measurement.main_details.delay,
+        measurement.main_details.stratum, measurement.main_details.precision,
+        measurement.main_details.reachability,
+        measurement.extra_details.root_delay.seconds, measurement.extra_details.ntp_last_sync_time.seconds,
+        measurement.extra_details.root_delay.fraction, measurement.extra_details.ntp_last_sync_time.fraction
+    ))
+
+    conn.commit()
+    cur.close()
+    conn.close()
 
 
---
+++ server/tests/test_db.py
@@ -0,0 +1,23 @@
+from ipaddress import IPv6Address, IPv4Address
+
+from server.app.models.NtpExtraDetails import NtpExtraDetails
+from server.app.models.NtpMainDetails import NtpMainDetails
+from server.app.models.NtpMeasurement import NtpMeasurement
+from server.app.models.NtpServerInfo import NtpServerInfo
+from server.app.models.NtpTimestamps import NtpTimestamps
+from server.app.models.PreciseTime import PreciseTime
+from server.app.db.connection import insert_measurement
+
+def test_insert_object():
+    t1 = PreciseTime(10000, 0)
+    t2 = PreciseTime(10002, 2**27)
+    t3 = PreciseTime(10003, 10000)
+    t4 = PreciseTime(10004, 10000)
+    server_details = NtpServerInfo(3,IPv4Address('192.0.2.1'),"local",IPv6Address('2001:db8::1'),"reference")
+    times = NtpTimestamps(t1,t2,t3,t4)
+    main_details = NtpMainDetails(0.009,0,1,0,"stable")
+    extra = NtpExtraDetails(PreciseTime(100000,0),PreciseTime(100000,0),0)
+
+    m = NtpMeasurement(server_details,times,main_details,extra)
+
+    insert_measurement(m)
