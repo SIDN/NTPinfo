424b489ded02caf6b336d38248d29d2d34469307
Mihai Nicolae <m.v.nicolae@student.tudelft.nl>
2025-05-02 14:11:01 +0200

    Several tweaks to fix style

--- client/src/components/SearchBar.tsx
+++ client/src/components/SearchBar.tsx
@@ -17,17 +17,24 @@ const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
     };
 
     return (
-        <div className="search-bar">
-            <input
-                type="text"
-                value={query}
-                onChange={handleInputChange}
-                placeholder="time.google.com"
-            />
-            <button onClick={handleSearch}>
-                Measure
-            </button>
-        </div>
+        <>
+            <div className = "search-bar-text">
+                <p>
+                    Enter the domanin name or IP address of the NTP server you want to measure.
+                </p>
+            </div>
+            <div className="search-bar">
+                <input
+                    type="text"
+                    value={query}
+                    onChange={handleInputChange}
+                    placeholder="time.google.com"
+                />
+                <button onClick={handleSearch}>
+                    Measure
+                </button>
+            </div>
+        </>
     );
 };
 
