6524fd2c4d635e0f61b5fc8b424f503f8b8aa368
Horia Botezatu <h.a.botezatu@student.tudelft.nl>
2025-05-02 12:23:34 +0200

    Added connection pool

--- server/app/db/config.py
+++ server/app/db/config.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 import os
+from psycopg_pool import ConnectionPool
 
 load_dotenv()
 
@@ -10,3 +11,9 @@ DB_CONFIG = {
     'host': os.getenv('DB_HOST'),
     'port': os.getenv('DB_PORT')
 }
+# convert DB_CONFIG (a dict) to a connection string
+dsn = f"postgresql://{DB_CONFIG['user']}:{DB_CONFIG['password']}@{DB_CONFIG['host']}:{DB_CONFIG['port']}/{DB_CONFIG['dbname']}"
+# max_size represents the maximum number of simultaneous database connections
+# the pool is allowed to open and manage at once
+# additional requests will wait (block) until a connection becomes available, unless a timeout is reached
+pool = ConnectionPool(conninfo=dsn, max_size=5)
--- server/app/db/connection.py
+++ server/app/db/connection.py
@@ -1,59 +1,57 @@
-import psycopg2
-from server.app.db.config import DB_CONFIG
+from server.app.db.config import pool
 from server.app.models.NtpMeasurement import NtpMeasurement
 
-
+# inserts measurements in the database
 def insert_measurement(measurement : NtpMeasurement) :
 
-    conn = psycopg2.connect(**DB_CONFIG)
-    cur = conn.cursor()
+    # uses a connection pool because connecting everytime
+    # to the database is inefficient and can quickly exhaust resource
+    with pool.connection() as conn :
+        # if anything fails inside the transaction() block, it rolls back.
+        # otherwise, it commits when the block exits cleanly.
+        with conn.transaction() :
+            with conn.cursor() as cur :
 
-    cur.execute("""
-        INSERT INTO times (
-            client_sent, client_sent_prec,
-            server_recv, server_recv_prec,
-            server_sent, server_sent_prec,
-            client_recv, client_recv_prec
-        )
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
-        RETURNING id
-    """, (
-        measurement.timestamps.client_sent_time.seconds, measurement.timestamps.client_sent_time.fraction,
-        measurement.timestamps.server_recv_time.seconds, measurement.timestamps.server_recv_time.fraction,
-        measurement.timestamps.server_sent_time.seconds, measurement.timestamps.server_sent_time.fraction,
-        measurement.timestamps.client_recv_time.seconds, measurement.timestamps.client_recv_time.fraction
-    ))
+                cur.execute("""
+                    INSERT INTO times (
+                        client_sent, client_sent_prec,
+                        server_recv, server_recv_prec,
+                        server_sent, server_sent_prec,
+                        client_recv, client_recv_prec
+                    )
+                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+                    RETURNING id
+                """, (
+                    measurement.timestamps.client_sent_time.seconds, measurement.timestamps.client_sent_time.fraction,
+                    measurement.timestamps.server_recv_time.seconds, measurement.timestamps.server_recv_time.fraction,
+                    measurement.timestamps.server_sent_time.seconds, measurement.timestamps.server_sent_time.fraction,
+                    measurement.timestamps.client_recv_time.seconds, measurement.timestamps.client_recv_time.fraction
+                ))
 
-    time_id = cur.fetchone()[0]
-    print(time_id)
+                # used because we have it as a foreign key in the measurements table
+                time_id = cur.fetchone()[0]
 
-    cur.execute("""
-        INSERT INTO measurements(
-            ntp_server_ip, ntp_server_name,
-            ntp_version, ntp_server_ref_parent,
-            ref_name, time_id,
-             "offset", delay,
-            stratum, precision,
-            reachability, 
-            root_delay,
-            ntp_last_sync_time,
-            root_delay_prec,
-            ntp_last_sync_time_prec
-        )
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-    """, (
-        str(measurement.server_info.ntp_server_ip), measurement.server_info.ntp_server_name,
-        measurement.server_info.ntp_version, str(measurement.server_info.ntp_server_ref_parent_ip),
-        measurement.server_info.ref_name, time_id,
-        measurement.main_details.offset, measurement.main_details.delay,
-        measurement.main_details.stratum, measurement.main_details.precision,
-        measurement.main_details.reachability,
-        measurement.extra_details.root_delay.seconds, measurement.extra_details.ntp_last_sync_time.seconds,
-        measurement.extra_details.root_delay.fraction, measurement.extra_details.ntp_last_sync_time.fraction
-    ))
-
-    conn.commit()
-    cur.close()
-    conn.close()
-
-
+                cur.execute("""
+                    INSERT INTO measurements(
+                        ntp_server_ip, ntp_server_name,
+                        ntp_version, ntp_server_ref_parent,
+                        ref_name, time_id,
+                        time_offset, delay,
+                        stratum, precision,
+                        reachability, 
+                        root_delay,
+                        ntp_last_sync_time,
+                        root_delay_prec,
+                        ntp_last_sync_time_prec
+                    )
+                        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                """, (
+                    measurement.server_info.ntp_server_ip, measurement.server_info.ntp_server_name,
+                    measurement.server_info.ntp_version, measurement.server_info.ntp_server_ref_parent_ip,
+                    measurement.server_info.ref_name, time_id,
+                    measurement.main_details.offset, measurement.main_details.delay,
+                    measurement.main_details.stratum, measurement.main_details.precision,
+                    measurement.main_details.reachability,
+                    measurement.extra_details.root_delay.seconds, measurement.extra_details.ntp_last_sync_time.seconds,
+                    measurement.extra_details.root_delay.fraction, measurement.extra_details.ntp_last_sync_time.fraction
+                ))
