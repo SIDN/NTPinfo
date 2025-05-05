f9a2d4cb60b4ae1fe8be5786bbb42237abd7b25f
Calin Olaru <c.m.olaru@student.tudelft.nl>
2025-05-05 15:01:13 +0200

    additionally removed the methods from the data classes

--- server/app/models/PreciseTime.py
+++ server/app/models/PreciseTime.py
@@ -11,7 +11,3 @@ class PreciseTime:
     """
     seconds:int
     fraction:int
-
-    def calculate_float_time(self)->float:
-        #seconds+fraction/2^32
-        return self.seconds+self.fraction/2**32
