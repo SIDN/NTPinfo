17ac19a397b6babd0143dcfdfe4093c5fc3ecf69
Calin Olaru <c.m.olaru@student.tudelft.nl>
2025-05-30 14:04:54 +0200

    Changed frontend to support multiple tabs (switching not yet implemented); Moved download functions to different file

---
+++ client/src/tabs/HomeTab.tsx
@@ -0,0 +1,175 @@
+import { useState } from 'react'
+
+import '../styles/HomeTab.css'
+import InputSection from '../components/InputSection.tsx'
+import ResultSummary from '../components/ResultSummary'
+import DownloadButton from '../components/DownloadButton'
+import VisualizationPopup from '../components/Visualization'
+import LineChart from '../components/LineGraph'
+import { useFetchIPData } from '../hooks/useFetchIPData.ts'
+import { useFetchHistoricalIPData } from '../hooks/useFetchHistoricalIPData.ts'
+import { dateFormatConversion } from '../utils/dateFormatConversion.ts'
+import {downloadJSON, downloadCSV} from '../utils/downloadFormats.ts'
+import WorldMap from '../components/WorldMap.tsx'
+
+import { NTPData } from '../utils/types.ts'
+import { Measurement } from '../utils/types.ts'
+import { LatLngTuple } from 'leaflet'
+
+import 'leaflet/dist/leaflet.css'
+
+function HomeTab() {
+
+    //
+    // states we need to define 
+    //
+    const [ntpData, setNtpData] = useState<NTPData | null>(null)
+    const [chartData, setChartData] = useState<NTPData[] | null>(null)
+    const [measured, setMeasured] = useState(false)
+    const [popupOpen, setPopupOpen] = useState(false)
+    const [selOption1, setOption1] = useState("Last Hour")
+    const [selOption2, setOption2] = useState("Hours")
+    const [selMeasurement, setSelMeasurement] = useState<Measurement>("offset")
+    
+    //Variables to log and use API hooks
+    const {fetchData: fetchMeasurementData, loading: apiDataLoading, error: apiErrorLoading, httpStatus: respStatus} = useFetchIPData()
+    const {fetchData: fetchHistoricalData, loading: apiHistoricalLoading, error: apiHistoricalError} = useFetchHistoricalIPData()
+
+    
+
+    const probes: [NTPData, LatLngTuple][] = [ 
+        [{ offset: 12.5, RTT: 34.2, stratum: 2, jitter: 0.3, precision: -20, status: "synced", time: 1716895200, ip: "192.168.1.10", ip_list: ["192.168.1.11", "192.168.1.12"], server_name: "ntp1.local", ref_ip: "192.168.1.1", ref_name: "ref1.local", root_delay: 1.2 },[51.51, -0.1]],
+        [{ offset: -7.3, RTT: 28.4, stratum: 3, jitter: null, precision: -18, status: "unsynced", time: 1716895300, ip: "10.0.0.5", ip_list: ["10.0.0.6"], server_name: "ntp2.domain", ref_ip: "10.0.0.1", ref_name: "ref2.domain", root_delay: 0.8 },[51.52, -0.08]],
+        [{ offset: 0.0, RTT: 45.1, stratum: 1, jitter: 0.1, precision: -19, status: "synced", time: 1716895400, ip: "172.16.0.2", ip_list: ["172.16.0.3", "172.16.0.4"], server_name: "ntp3.network", ref_ip: "172.16.0.1", ref_name: "ref3.network", root_delay: 0.5 },[51.5, -0.07]]
+    ];
+
+
+    const ntpServer: LatLngTuple = [51.53, -0.09];
+
+    //dropdown format
+    // second one will removed after custom time intervals are added
+    const dropdown = [
+    {
+      label: "Time period",
+      options: ["Last Hour", "Last Day", "Last Week", "Custom"],
+      selectedValue: selOption1,
+      onSelect: setOption1,
+      className: "time-dropdown"
+    },
+    {
+      label: "Time Option",
+      options: ["Hours", "Days"],
+      selectedValue: selOption2,
+      onSelect: setOption2,
+      className: "custom-time-dropdown"
+    }
+    ]
+
+    //
+  //functions for handling state changes
+  //
+  
+  //main function called when measuring by pressing the button
+  const handleSearch = async (query: string, jitter_flag: boolean, measurements_no: number) => {
+    if (query.length == 0)
+      return
+    
+    const payload = {
+      server: query,
+      jitter_flag: jitter_flag,
+      measurements_no: jitter_flag ? measurements_no : 0
+    }
+
+    // Get the response from the measurement data API
+    const fullurlMeasurementData = `http://localhost:8000/measurements/`
+    const apiMeasurementResp = await fetchMeasurementData(fullurlMeasurementData, payload)
+
+    //Get data from past day from historical data API to chart in the graph
+    const startDate = dateFormatConversion(Date.now()-86400000)
+    const endDate = dateFormatConversion(Date.now())
+    const fullurlHistoricalData = `http://localhost:8000/measurements/history/?server=${query}&start=${startDate}&end=${endDate}`
+    const apiHistoricalResp = await fetchHistoricalData(fullurlHistoricalData)
+    
+    //update data stored and show the data again
+    setMeasured(true)
+    const data = apiMeasurementResp
+    const chartData = apiHistoricalResp
+    setNtpData(data ?? null)
+    setChartData(chartData ?? null)
+  }
+
+  //function to determine what value to use on the y axis of the graph
+  const handleMeasurementChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSelMeasurement(event.target.value as Measurement);
+  }
+    return (
+    <>
+        <div className="search-wrapper">
+        <InputSection onSearch={handleSearch} />
+      </div>
+        <div className="result-text">
+          {(!apiDataLoading && measured && (<p>Results</p>)) || (apiDataLoading && <p>Loading...</p>)}
+        </div>
+      {(ntpData && !apiDataLoading && (<div className="results-and-graph">
+        <ResultSummary data={ntpData} err={apiErrorLoading} httpStatus={respStatus}/>
+       
+        <div className="graphs">
+          <div className='graph-box'>
+            <label>
+              <input
+                type="radio"
+                name="measurement"
+                value="offset"
+                checked={selMeasurement === 'offset'}
+                onChange={handleMeasurementChange}
+              />
+              Offset
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="measurement"
+                value="RTT"
+                checked={selMeasurement === 'RTT'}
+                onChange={handleMeasurementChange}
+              />
+              Round-trip time
+            </label>
+            <LineChart data = {chartData} selectedMeasurement={selMeasurement} selectedOption="Last Day"/>
+          </div>
+        </div>
+        <div className='map-box'>
+          <WorldMap probes={probes} ntpServer={ntpServer}/>
+        </div>
+      </div>)) || (!ntpData && !apiDataLoading && measured && <ResultSummary data={ntpData} err={apiErrorLoading} httpStatus={respStatus}/>)}
+      
+      {/*Only shown when a domain name is queried. Users can download IP addresses corresponding to that domain name*/}
+      {ntpData && !apiDataLoading && ntpData.server_name && ntpData.ip_list.length && (() => {
+
+                const downloadContent = `Server name: ${ntpData.server_name}\n\n${ntpData.ip_list.join('\n')}`
+                const blob = new Blob([downloadContent], { type: 'text/plain' })
+                const downloadUrl = URL.createObjectURL(blob)
+               return (<p className="ip-list">You can download more IP addresses corresponding to this domain name  
+               <span> <a href={downloadUrl} download="ip-list.txt">here</a></span>
+                </p>)
+            })()}
+      
+      {/*Buttons to download results in JSON and CSV format as well as open a popup displaying historical data*/}
+      {ntpData && !apiDataLoading && (<div className="download-buttons">
+      
+        <DownloadButton name="Download JSON" onclick={() => downloadJSON({data : [ntpData]})} />
+        <DownloadButton name="Download CSV" onclick={() => downloadCSV({data : [ntpData]})} />
+        <div>
+          <button className="open-popup-btn" onClick={() => setPopupOpen(true)}>View Historical Data</button>
+          <VisualizationPopup
+          isOpen={popupOpen}
+          onClose={() => setPopupOpen(false)}
+          dropdowns={dropdown}
+          data = {chartData}/>
+        </div>
+      </div>)}
+    </>
+    )
+}
+
+export default HomeTab
