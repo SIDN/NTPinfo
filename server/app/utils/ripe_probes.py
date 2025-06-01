fcd7e344db1251a69eb0096161695837eecf4ba9
Serban Orza <orza@student.tudelft.nl>
2025-06-01 15:44:57 +0200

    added the logic for distributing the probes. Now we need to add the final step (solving the problem of float errors)

--- server/app/utils/ripe_probes.py
+++ server/app/utils/ripe_probes.py
@@ -20,6 +20,7 @@ def get_probes(ntp_server_ip: str, probes_requested: int=30) -> list[dict]:
         list[dict]: The list of probes that we will use for the measurement.
     """
     # get the details. (this will take around 150-200ms)
+    ip_family: int = get_ip_family(ntp_server_ip)
     ip_asn, ip_country, ip_area = get_ip_network_details(ntp_server_ip)
     ip_prefix = get_prefix_from_ip(ntp_server_ip)
     # settings:
@@ -36,7 +37,7 @@ def get_probes(ntp_server_ip: str, probes_requested: int=30) -> list[dict]:
     # But for now we use a default order. We would change this logic in future
 
     best_probe_types: dict[str, int] = get_best_probe_types(ip_asn=ip_asn, ip_prefix=ip_prefix, ip_country=ip_country,
-                                                            ip_area=ip_area, probes_requested=probes_requested)
+                                                ip_area=ip_area, ip_family=ip_family, probes_requested=probes_requested)
 
     for probe_type, n in best_probe_types.items():
         # instead of a switch, I used a map with lambda methods
@@ -47,7 +48,7 @@ def get_probes(ntp_server_ip: str, probes_requested: int=30) -> list[dict]:
 
 
 def get_best_probe_types(ip_asn: Optional[str], ip_prefix: Optional[str], ip_country: Optional[str],
-               ip_area: Optional[str], probes_requested: int=30) -> dict[str, int]:
+               ip_area: Optional[str], ip_family: int, probes_requested: int=30) -> dict[str, int]:
     """
     This method is responsible for getting the best probes for the measurement. It should return probes that
     are near the NTP server.
@@ -57,6 +58,7 @@ def get_best_probe_types(ip_asn: Optional[str], ip_prefix: Optional[str], ip_cou
         ip_prefix (str): The prefix of the NTP server IP address.
         ip_country (str): The country of the NTP server IP address.
         ip_area (str): The area of the NTP server IP address.
+        ip_family (int): The family of the NTP server IP address. (4 or 6)
         probes_requested (int): The total number of probes that we will request.
 
     Returns:
@@ -65,6 +67,8 @@ def get_best_probe_types(ip_asn: Optional[str], ip_prefix: Optional[str], ip_cou
     # for now, we have a default logic! This method is not the final version.
     # the best distribution:
     # 33% 30% 27% 10% 0%
+    max_pbs = 100
+    # If the limit for a type is reached, we cannot increase the number of the probes of this type.
     limit_reached = {
         "asn": False,
         "prefix": False,
@@ -72,73 +76,87 @@ def get_best_probe_types(ip_asn: Optional[str], ip_prefix: Optional[str], ip_cou
         "area": False,
         "random": False
     }
-    probes_wanted = {
-        "asn": int(probes_requested * 33 / 100),
-        "prefix": int(probes_requested * 30 / 100),
-        "country": int(probes_requested * 27 / 100),
-        "area": int(probes_requested * 10 / 100),
-        "random": 0
-    }
-    distribution_functions ={
-        "asn": lambda i: ([0,30,50,20,0])[i],
-        "prefix": lambda i: ([40,0,40,20,0])[i],
-        "country": lambda i: ([50,30,0,20,0])[i],
-        "area": lambda i: ([20,0,20,0,60])[i],
-    }
+    # probes_wanted = {
+    #     "asn": int(probes_requested * 33 / 100),
+    #     "prefix": int(probes_requested * 30 / 100),
+    #     "country": int(probes_requested * 27 / 100),
+    #     "area": int(probes_requested * 10 / 100),
+    #     "random": 0
+    # }
+    # distribution_functions = {
+    #     "asn": lambda i: ([0,30,50,20,0])[i],
+    #     "prefix": lambda i: ([40,0,40,20,0])[i],
+    #     "country": lambda i: ([50,30,0,20,0])[i],
+    #     "area": lambda i: ([20,0,20,0,60])[i],
+    # }
+    #daca un field nu e->available of sa fie 0
+    #wanted  13.3,  11.3,   10.2,   4
+    #available: 9,     2,    606, 100  -> ans: 9,?,?,?
+    #ramase:  4.3,   9.3,       0,  0
+    #4.3->distribute equally to the others except random
+    #wanted     x    12.73   11.63, 5.43
+    #ramase:         10.73
+    #split 10.73:               5
+    #
+
+    #wanted   13.3,  11.3,   10.2,   4
+    #available: 15,     2,    606, 100  -> ans: 9,?,?,?
+    probes_wanted_percentages=[0.33, 0.3, 0.27, 0.10, 0.0]
+    probes_wanted = [13.3, 11.3, 10.27, 4.2, 0.0]
+
+    # "ans" means how many probes of each type we will ask for
+    # type 0 is ASN, type 1 is prefix, type 2 is country code, type 3 is area and type 4 is random
+    ans: list[float] = [0, 0, 0, 0, 0] # how many probes
+    # wanted     0,  11.3,   10.2,   4
+    # available: 3,     2,    606, 100  -> ans: 9,?,?,?
+    # remains:    0,   9.3,       0,  0
+            # 0 11.3 10.27
+            # 3   2   606
+    ip_type = "ipv" + str(ip_family)
     # see what inputs we received and with what we start:
-    if ip_asn is None:
-        limit_reached["asn"] = True
-        probes_wanted = distribute_probes_to_others(probes_wanted, limit_reached, distribution_functions["asn"])
+    probes_available: list[float] = [0, 0, 0, 0, max_pbs]
+    # see what is available on RIPE Atlas
+    if ip_asn is not None:
+        probes_available[0] = get_available_probes_asn(ip_asn, ip_type)
     if ip_prefix is not None:
-        limit_reached["prefix"] = True
-        probes_wanted = distribute_probes_to_others(probes_wanted, limit_reached, distribution_functions["prefix"])
+        probes_available[1] = get_available_probes_prefix(ip_prefix, ip_type)
     if ip_country is not None:
-        limit_reached["country"] = True
-        probes_wanted = distribute_probes_to_others(probes_wanted, limit_reached, distribution_functions["country"])
+        probes_available[2] = get_available_probes_country(ip_country, ip_type)
     if ip_area is not None:
-        limit_reached["area"] = True
-        probes_wanted = distribute_probes_to_others(probes_wanted, limit_reached, distribution_functions["area"])
+        probes_available[3] = max_pbs # there are enough probes in an area for sure.
+    # for the types that the available number of probes is less than how many we want:
+    for i in range(0,len(probes_wanted)):
+        print("step: ",i)
+        print(probes_wanted)
+        print(probes_available)
+        print(ans)
+        print("===========")
+        if probes_wanted[i] > probes_available[i]: # we want more than we have
+            # we need probes from other types, but first take what we found in the desired type
+            ans[i] += probes_available[i]
+            needed: float = probes_wanted[i] - probes_available[i]
+            probes_wanted[i] -= probes_available[i]
+            probes_available[i] = 0
+            # take from other types and update the available probes
+            probes_available, ans = take_from_available_probes(needed, probes_available, ans)
+            # we take what we needed
+            #ans[i] += needed
+            probes_wanted[i] = 0
+        else:
+            # take the desired number of probes as we have enough
+            probes_available[i] -= probes_wanted[i]
+            ans[i] += probes_wanted[i]
+            probes_wanted[i]=0
 
-    #see what is available on RIPE Atlas
-    available_probes_asn = ping_asn_probes(probes_wanted["asn"])
-    available_probes_prefix = ping_prefix_probes(probes_wanted["prefix"])
-    available_probes_country = ping_country_probes(probes_wanted["country"])
+        print(probes_wanted)
+        print(probes_available)
+        print(ans)
 
-    # prefix (the most expected to fail)
-    pbs_to_distribute = probes_wanted["prefix"] - available_probes_prefix
-    if pbs_to_distribute > 0:
-        # modify probes_wanted
-        limit_reached["prefix"] = True
-        probes_wanted = distribute_probes_to_others(probes_wanted, limit_reached, lambda: [40,0,40,20,0])
-    # asn
-    pbs_to_distribute = probes_wanted["asn"] - available_probes_asn
-    if pbs_to_distribute > 0:
-        # modify probes_wanted
-        limit_reached["asn"] = True
-        probes_wanted = distribute_probes_to_others(probes_wanted, limit_reached, lambda c: [0,30,50,20,0])
-    # country
-    pbs_to_distribute = probes_wanted["country"] - available_probes_country
-    if pbs_to_distribute > 0:
-        # modify probes_wanted
-        limit_reached["country"] = True
-        probes_wanted = distribute_probes_to_others(probes_wanted, limit_reached, lambda c: [50,30,0,20,0])
 
     #area would probably not fail as it probably contains more than 100 probes
     #we save the time to ping it here.
-
+    print("the answer is:",ans)
     best_probe_types: dict[str, int] = {"random": int(probes_wanted["random"])} # default
-    for probe_type, n in probes_wanted.items():
-        if n > 0:
-            best_probe_types[probe_type] = int(n)
-    # if ip_asn is not None:
-    #     best_probe_types["asn"] = int(probes_wanted["asn"])
-    # if ip_prefix is not None:
-    #     best_probe_types["prefix"] = int(probes_wanted["prefix"])
-    # if ip_country is not None:
-    #     best_probe_types["country"] = int(probes_wanted["country"])
-    # if ip_area is not None:
-    #     best_probe_types["area"] = int(probes_wanted["area"])
-
     return best_probe_types
 
 
@@ -242,58 +260,27 @@ def get_country_probes(ip_country_code: Optional[str], n: int) -> dict:
         }
     return probes
 
-def ping_probes(ntp_server_ip: str, probes: dict[str|int]) ->int:
-    api_key = get_ripe_api_token()
-    headers = {
-        "Authorization": f"Key {api_key}",
-        "Content-Type": "application/json"
-    }
-    request_content = {"definitions": [
-        {
-            "type": "ping",
-            "af": get_ip_family(ntp_server_ip),
-            "resolve_on_probe": True,
-            "description": f"Ping measurement to {ntp_server_ip}",
-            "packets": 3,
-            "size": 48,
-            "skip_dns_check": False,
-            "include_probe_id": False,
-            "target": ntp_server_ip,
-            "timeout": 1500,
-        }
-    ],
-        "is_oneoff": True,
-        "bill_to": get_ripe_account_email(),
-        "probes": probes
-    }
-    response = requests.post(
-        "https://atlas.ripe.net/api/v2/measurements/",
-        headers=headers,
-        data=json.dumps(request_content)
-    )
-    data = response.json()
-    # the answer has a list of measurements, but we only did one measurement so we send one.
-    ans: str = data["measurements"][0]
-    return 2
 def get_available_probes_asn(ip_asn: str, ip_type: str) -> int:
     """
     This method selects n probes that has the same prefix and supports ipv4 or ipv6, it depends on the type.
     Args:
         ip_asn (str): the ASN of the searched network
-        ip_type (str): the IP type (ipv4 or ipv6). It should be lowercase.
+        ip_type (str): the IP type (ipv4 or ipv6). (not case-sensitive)
     Returns:
         int: the number of available probes
     """
     # in wsl, this command would be for example:
     # ripe-atlas probe-search --prefix NL --status 1 --tag system-ipv4-works
+    ip_asn_number=int(ip_asn[2:])
+    print("asn number:", ip_asn_number)
     filters = {
-        "asn": ip_asn,
+        "asn": ip_asn_number,
         "status": 1,  # Connected probes
         "tags": f"system-{ip_type.lower()}-works",
     }
     probes = ProbeRequest(
         return_objects=False,
-        page_size=1, # we are only interested in the number. If you want the probes, use 100 here
+        page_size=1, # we are only interested in the number. If you want the id of the probes, use 100 here
         **filters,
     )
     try:
@@ -322,13 +309,13 @@ def get_available_probes_prefix(ip_prefix: str, ip_type: str) -> int:
     }
     probes = ProbeRequest(
         return_objects=False,
-        page_size=1, # we are only interested in the number. If you want the probes, use 100 here
+        page_size=1, # we are only interested in the number. If you want the id of the probes, use 100 here
         **filters,
     )
     try:
         #this fetches after the first request
         next(probes) # we need to trigger the probes because "probes" does not get populated immediately.
-    except Exception:# | StopIteration:
+    except StopIteration:
         return 0 # error
     ans: int = probes.total_count
     return ans
@@ -351,7 +338,7 @@ def get_available_probes_country(country_code: str, ip_type: str) -> int:
     probes = ProbeRequest(
         return_objects=False,
         #fields="id",
-        page_size=1, # we are only interested in the number. If you want the probes, use 100 here
+        page_size=1, # we are only interested in the number. If you want the id of the probes, use 100 here
         **filters,
     )
     try:
@@ -363,13 +350,41 @@ def get_available_probes_country(country_code: str, ip_type: str) -> int:
     ans: int = probes.total_count
     return ans
 
+def take_from_available_probes(needed: float, probes_available: list[float],
+                               ans: list[float]) -> tuple[list[float], list[float]]:
+    # for clarity, "needed" means how many probes we need to take from other categories
+    ans_for_index=0
+    for i in range(0,len(probes_available)):
+        # if we have something there that we could take
+        if probes_available[i] > 0:
+            if probes_available[i] < needed: # if we have less than we want, take everything from that source
+                # we will take "probes_available[i]" probes from type i
+                ans[i] += probes_available[i]
+                # subtract what we found
+                needed -= probes_available[i]
+                # we emptied this source
+                probes_available[i] = 0
+            else:
+                # we have more than "needed" in probes_available[i]
+                # we will take "needed" probes from type i
+                ans[i] += needed
+                # remove them from available
+                probes_available[i] -= needed
+                needed=0
+                # stop as we finished our job there
+                break
+    if needed > 0:
+        raise Exception("Not enough probes available")
+    # ans_for_index would always be the initial value of needed
+    return probes_available, ans
 
 import time
 
 start = time.time()
-print(get_available_probes_asn("9009","ipv4"))
+print(get_available_probes_asn("AS9009","ipv4"))
 print(get_available_probes_prefix("80.211.224.0/16","ipv4"))
 print(get_available_probes_country("NL","ipv4"))
+get_best_probe_types("AS9009","80.211.224.0/20","NL","da",4)
 end = time.time()
 print(end - start)
 
@@ -381,11 +396,7 @@ def ping_prefix_probes(probes_wanted: int) ->int:
 def ping_country_probes(probes_wanted: int) ->int:
     return 9
 
-def distribute_probes_to_others(probes_wanted, limited_reach, f) -> dict:
-    #asn_pbs+=func(asn_pbs)
-    if not limited_reach["asn"]:
-        probes_wanted["asn"] += f(0)
-    return probes_wanted
+
 #use cases:
 # import pprint
 # pprint.pprint(get_probes("AS15169","192.2.3.0/8","IT", "West",20))
