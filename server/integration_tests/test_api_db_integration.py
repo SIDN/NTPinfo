df481c80d063b1d39a870974f467ba93a09048cf
Calin <c.m.olaru@student.tudelft.nl>
2025-06-09 16:09:41 +0200

    added tests for exceptions / edge cases

--- server/integration_tests/test_api_db_integration.py
+++ server/integration_tests/test_api_db_integration.py
@@ -113,4 +113,70 @@ def test_perform_multiple_measurement_rate_limiting(client):
     assert resp_get_after.status_code == 200
     assert len(resp_get_after.json()["measurements"]) == 5
 
+def test_perform_measurement_empty_server(client):
+    headers = {"X-Forwarded-For": "83.25.24.10"}
+
+    response = client.post("/measurements/", json={"server": ""},
+                                headers=headers)
+    assert response.status_code == 400
+    assert response.json() == {'error': "Either 'ip' or 'dn' must be provided"}
+
+    end = datetime.now(timezone.utc)
+    resp_get_after = client.get("/measurements/history/", params={
+        "server": "91.210.128.220",
+        "start": (end - timedelta(minutes=10)).isoformat(),
+        "end": end.isoformat()
+    })
+    assert len(resp_get_after.json()["measurements"]) == 0
+
+def test_perform_measurement_wrong_server(client):
+    headers = {"X-Forwarded-For": "83.25.24.10"}
+
+    response = client.post("/measurements/", json={"server": "random-server"},
+                                headers=headers)
+    assert response.status_code == 404
+    assert "measurement" not in response.json()
+
+    end = datetime.now(timezone.utc)
+    resp_get_after = client.get("/measurements/history/", params={
+        "server": "random-server",
+        "start": (end - timedelta(minutes=10)).isoformat(),
+        "end": end.isoformat()
+    })
+    assert len(resp_get_after.json()["measurements"]) == 0
+
+def test_read_historic_data_empty(client):
+    end = datetime.now(timezone.utc)
+    response = client.get("/measurements/history/", params={
+        "server": "",
+        "start": (end - timedelta(minutes=10)).isoformat(),
+        "end": end.isoformat()
+    })
+    assert response.status_code == 400
+    assert "measurement" not in response.json()
+    assert response.json() == {'error': "Either 'ip' or 'domain name' must be provided"}
+
+def test_read_historic_data_wrong_start(client):
+    end = datetime.now(timezone.utc)
+    response = client.get("/measurements/history/", params={
+        "server": "91.210.128.220",
+        "start": (end + timedelta(minutes=10)).isoformat(),
+        "end": end.isoformat()
+    })
+    assert response.status_code == 400
+    assert "measurement" not in response.json()
+    assert response.json() == {'error': "'start' must be earlier than 'end'"}
+
+def test_read_historic_data_wrong_end(client):
+    end = datetime.now(timezone.utc) + timedelta(minutes=10)
+    response = client.get("/measurements/history/", params={
+        "server": "91.210.128.220",
+        "start": (end - timedelta(minutes=10)).isoformat(),
+        "end": end.isoformat()
+    })
+    assert response.status_code == 400
+    assert "measurement" not in response.json()
+    assert response.json() == {'error': "'end' cannot be in the future"}
+
+
 
