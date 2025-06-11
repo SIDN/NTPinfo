823efc660c43bea75443ab58a165ffbcd7df4829
Serban Orza <orza@student.tudelft.nl>
2025-06-12 00:19:23 +0200

    Solved some typing issues

--- server/app/utils/location_resolver.py
+++ server/app/utils/location_resolver.py
@@ -7,7 +7,7 @@ from server.app.utils.load_config_data import get_max_mind_path_asn
 from server.app.utils.load_config_data import get_max_mind_path_country, get_max_mind_path_city
 
 
-def get_coordinates_for_ip(client_ip: str) -> tuple[float, float]:
+def get_coordinates_for_ip(client_ip: Optional[str]) -> tuple[float, float]:
     """
     Retrieves the geographical location (latitude and longitude) of a given IP address
     using the MaxMind GeoLite2-City database.
@@ -16,12 +16,14 @@ def get_coordinates_for_ip(client_ip: str) -> tuple[float, float]:
     the IP not being found, it returns the coordinates of an (almost) randomly chosen location as a fallback.
 
     Args:
-        client_ip (str): The IP address of the client to geolocate
+        client_ip (Optional[str]): The IP address of the client to geolocate
 
     Returns:
         tuple[float, float]: A tuple containing the latitude and longitude. If the location
         cannot be resolved, returns (25.0, -71.0)
     """
+    if client_ip is None:
+        return 25.0, -71.0
     try:
         with geoip2.database.Reader(f'{get_max_mind_path_city()}') as reader:
             response = reader.city(client_ip)
@@ -35,18 +37,20 @@ def get_coordinates_for_ip(client_ip: str) -> tuple[float, float]:
         return 25.0, -71.0
 
 
-def get_country_for_ip(client_ip: str) -> Optional[str]:
+def get_country_for_ip(client_ip: Optional[str]) -> Optional[str]:
     """
     Retrieves the country code of a given IP address using the MaxMind GeoLite2-Country database.
     If the country code cannot be determined due to missing data, database issues, or
     the IP not being found, it returns None
 
     Args:
-        client_ip (str): The IP address of the client to geolocate
+        client_ip (Optional[str]): The IP address of the client to geolocate
 
     Returns:
         Optional[str]: the country code for the ip location or None
     """
+    if client_ip is None:
+        return None
     try:
         with geoip2.database.Reader(f'{get_max_mind_path_country()}') as reader:
             response = reader.country(client_ip).country.iso_code
@@ -56,18 +60,20 @@ def get_country_for_ip(client_ip: str) -> Optional[str]:
         return None
 
 
-def get_continent_for_ip(client_ip: str) -> Optional[str]:
+def get_continent_for_ip(client_ip: Optional[str]) -> Optional[str]:
     """
     Retrieves the continent code of a given IP address using the MaxMind GeoLite2-Country database.
     If the continent code cannot be determined due to missing data, database issues, or
     the IP not being found, it returns None
 
     Args:
-        client_ip (str): The IP address of the client to geolocate
+        client_ip (Optional[str]): The IP address of the client to geolocate
 
     Returns:
         Optional[str]: the continent code for the ip location or None
     """
+    if client_ip is None:
+        return None
     try:
         with geoip2.database.Reader(f'{get_max_mind_path_country()}') as reader:
             response = reader.country(client_ip).continent.code
