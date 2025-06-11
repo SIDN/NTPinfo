8f9faedaa831aefd11a5be18bfc73c2733577457
Mihai Nicolae <m.v.nicolae@student.tudelft.nl>
2025-06-11 14:53:15 +0200

    New logo and style changes

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -1,12 +1,14 @@
 import '../styles/Hero.css'
 import github from '../assets/github-logo-high-res-white.png'
+import NTPinfoLogo from '../assets/NTPinfo.png'
 
 function Hero() {
     return (
         <div className="hero">
             <div className="text-hero">
-                <h1 id="title">Are your time servers on time?</h1>
-                <p>A tool to evaluate the accuracy of publicly available NTP servers</p>
+                <img src={NTPinfoLogo} alt="NTPinfo Logo" className="ntpinfo-logo" />
+                <h1 id="title">NTPinfo</h1>
+                {/* <p>A tool to evaluate the accuracy of publicly available NTP servers</p> */}
             </div>
             <nav className="navbar">
                 {/* <a href="#" aria-label="About">About</a> */}
