5dbcd172f8d62576260245c52c6a43fca7ac8519
Mihai Nicolae <m.v.nicolae@student.tudelft.nl>
2025-06-14 18:24:37 +0200

    Removed duplicate styling for the graph box from App.css

--- client/src/components/Graphs.tsx
+++ client/src/components/Graphs.tsx
@@ -1,17 +1,3 @@
 // UNUSED
 
 import '../styles/Graphs.css'
-
-function Graphs() {
-    return (
-        <div className="graphs">
-            {/* <div className="graph-box">Offset</div>
-            <div className="graph-box">Delay</div> */}
-
-
-            <div className="graph-box">Graphs</div>
-        </div>
-    )
-}
-
-export default Graphs
