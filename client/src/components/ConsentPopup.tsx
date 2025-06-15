92f6ab0743305f756cd8e4c70161c76ba8206d6d
Calin <c.m.olaru@student.tudelft.nl>
2025-06-15 12:38:55 +0200

    added styling to popup

--- client/src/components/ConsentPopup.tsx
+++ client/src/components/ConsentPopup.tsx
@@ -5,13 +5,13 @@ export default function ConsentPopup() {
     const [showPopup, setShowPopup] = useState(false)
 
     useEffect(() => {
-        const consentGiven = localStorage.getItem('consentGiven')
-        if (!consentGiven)
+        const consentGiven = sessionStorage.getItem('consentGiven')
+        if (consentGiven === 'false' || !consentGiven)
             setShowPopup(true)
     }, [setShowPopup])
 
     const handleConsent = () => {
-    localStorage.setItem('consentGiven', 'true');
+    sessionStorage.setItem('consentGiven', 'true');
     setShowPopup(false);
     }
 
@@ -20,7 +20,7 @@ export default function ConsentPopup() {
             <div className="popup">
                 <div className="popup-card">
                     <h2>Privacy notice</h2>
-                    <p>Our application may need to use your IP to get more accurate location data.</p>
+                    <p>Our application may need to use your IP to <br></br>get more accurate geolocation data.</p>
                     <button onClick={handleConsent}>I consent</button>
                 </div>
             </div>
