111187872e5f079632ab292b27deada2c12c8fcb
Calin <c.m.olaru@student.tudelft.nl>
2025-06-15 17:41:19 +0200

    small changes to popup text

--- client/src/components/ConsentPopup.tsx
+++ client/src/components/ConsentPopup.tsx
@@ -20,7 +20,7 @@ export default function ConsentPopup() {
             <div className="popup">
                 <div className="popup-card">
                     <h2>Privacy notice</h2>
-                    <p>Our application may need to use your IP to <br></br>get more accurate geolocation data.</p>
+                    <p>Our application may need to use your IP address <br></br> to get more accurate geolocation data.<br></br>Your IP is masked and will never be stored.</p>
                     <button onClick={handleConsent}>I consent</button>
                 </div>
             </div>
