6846031831631ae03dbe404fe7fbaa3ab7c661bc
Mihai Nicolae <m.v.nicolae@student.tudelft.nl>
2025-06-18 00:21:47 +0200

    Extra details from measurements on the home page

--- client/src/components/ResultSummary.tsx
+++ client/src/components/ResultSummary.tsx
@@ -74,11 +74,16 @@ function ResultSummary({data, ripeData, err, httpStatus, ripeErr, ripeStatus} :
                     <div className="result-box" id="main-details">
                         <div className="metric"><span title='The difference between the time reported by the like an NTP server and your local clock'>Offset</span><span>{data?.offset ? `${(data.offset).toFixed(3)} ms` : 'N/A'} {offsetIconNTP && <img src={offsetIconNTP} alt="offset performance" style={{width:'14px',verticalAlign:'middle'}}/>}</span></div>
                         <div className="metric"><span title='The total time taken for a request to travel from the client to the server and back.'>Round-trip time</span><span>{data?.RTT ? `${(data.RTT).toFixed(3)} ms` : 'N/A'} {rttIconNTP && <img src={rttIconNTP} alt="rtt performance" style={{width:'14px',verticalAlign:'middle'}}/>}</span></div>
-                        <div className="metric"><span title='The variability in delay times between successive NTP messages, calculated as std. dev. of offsets'>Jitter</span><span>{data?.jitter ? `${(data.jitter).toFixed(3)} ms` : 'N/A'}</span></div>
+                        <div className="metric"><span title={`The variability in delay times between successive NTP messages, calculated as std. dev. of ${data?.nr_measurements_jitter} offsets`}>Jitter</span><span>{data?.jitter ? `${(data.jitter).toFixed(3)} ms` : 'N/A'}</span></div>
                         <div className="metric"><span title='The smallest time unit that the NTP server can measure or represent'>Precision</span><span>2<sup>{data?.precision}</sup></span></div>
                         <div className="metric"><span title='A hierarchical level number indicating the distance from the reference clock'>Stratum</span><span>{data?.stratum}</span></div>
                         <div className="metric"><span title='The IP address of the NTP server'>IP address</span><span>{data?.ip}</span></div>
                         <div className="metric"><span>Vantage point IP</span><span>{data?.vantage_point_ip}</span></div>
+                        <div className="metric"><span>Country</span><span>{data?.country_code}</span></div>
+                        <div className="metric"><span>Reference ID</span><span>{data?.ref_id}</span></div>
+                        <div className="metric"><span title='The total round-trip delay to the primary reference source'>Root delay</span><span>{data?.root_delay}</span></div>
+                        <div className="metric"><span title='The poll interval used by the probe during the measurement'>Poll</span><span>{data?.poll ? `${data.poll} s` : 'N/A'}</span></div>
+                        <div className="metric"><span title='An estimate of the maximum error due to clock frequency stability'>Root dispersion</span><span>{data?.root_dispersion ? `${(data.root_dispersion).toFixed(10)} s` : 'N/A'}</span></div>
                         <div className="status-line">
                             <span className="status-label">STATUS:&nbsp;</span>
                             <span className={`status-value ${status?.toLowerCase()}`}>{status}</span>
@@ -103,6 +108,11 @@ function ResultSummary({data, ripeData, err, httpStatus, ripeErr, ripeStatus} :
                         <div className="metric"><span title='A hierarchical level number indicating the distance from the reference clock'>Stratum</span><span>{ripeData?.measurementData.stratum}</span></div>
                         <div className="metric"><span title='The IP address of the NTP server'>IP address</span><span>{ripeData?.measurementData.ip}</span></div>
                         <div className="metric"><span>Vantage point IP</span><span>{ripeData?.measurementData.vantage_point_ip}</span></div>
+                        <div className="metric"><span>Country</span><span>{ripeData?.measurementData.country_code}</span></div>
+                        <div className="metric"><span>Reference ID</span><span>{ripeData?.measurementData.ref_id}</span></div>
+                        <div className="metric"><span title='The total round-trip delay to the primary reference source'>Root delay</span><span>{ripeData?.measurementData.root_delay}</span></div>
+                        <div className="metric"><span title='The poll interval used by the probe during the measurement'>Poll</span><span>{ripeData?.measurementData.poll ? `${ripeData.measurementData.poll} s` : 'N/A'}</span></div>
+                        <div className="metric"><span title='An estimate of the maximum error due to clock frequency stability'>Root dispersion</span><span>{ripeData?.measurementData.root_dispersion ? `${(ripeData.measurementData.root_dispersion).toFixed(10)} s` : 'N/A'}</span></div>
                         <div className="metric"><span>Measurement ID</span><span>
                             {ripeData?.measurement_id ? (
                                 <a
