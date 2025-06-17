8b6e9035c2e45fb25e1328fb128d6cd1a8ad7dd8
Horia Botezatu <h.a.botezatu@student.tudelft.nl>
2025-06-17 17:48:27 +0200

    Added unversioned script

---
+++ docker-entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "Current cron schedule is: ${UPDATE_CRON_SCHEDULE}"
+
+echo "${ACCOUNT_ID}"
+echo "${LICENSE_KEY}"
+
+echo "ACCOUNT_ID=${ACCOUNT_ID}" > /etc/cron.env
+echo "LICENSE_KEY=${LICENSE_KEY}" >> /etc/cron.env
+echo "" >> /etc/cron.env
+
+chmod 644 /etc/cron.env
+
+# setup cron job file with schedule and command
+echo "${UPDATE_CRON_SCHEDULE} . /etc/cron.env; /app/update.sh >> /var/log/cron.log 2>&1" > /etc/cron.d/updatejob
+
+# set permissions and register it
+chmod 0644 /etc/cron.d/updatejob
+crontab /etc/cron.d/updatejob
+
+# create cron log file
+touch /var/log/cron.log
+
+# start cron in background
+cron
+
+/app/update.sh
+# run fastapi app
+exec uvicorn server.app.main:create_app --factory --host 0.0.0.0 --port 8000
