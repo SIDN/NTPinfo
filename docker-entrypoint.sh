0f471b09feddb7d0d90b26182bcf337aee829ada
Horia Botezatu <h.a.botezatu@student.tudelft.nl>
2025-06-18 12:39:06 +0200

    Finished cron script run

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,9 +2,6 @@
 
 echo "Current cron schedule is: ${UPDATE_CRON_SCHEDULE}"
 
-echo "${ACCOUNT_ID}"
-echo "${LICENSE_KEY}"
-
 echo "ACCOUNT_ID=${ACCOUNT_ID}" > /etc/cron.env
 echo "LICENSE_KEY=${LICENSE_KEY}" >> /etc/cron.env
 echo "PATH=${PATH}" >> /etc/cron.env
@@ -23,6 +20,9 @@ touch /var/log/cron.log
 
 # start cron in background
 cron
+
+# run at start to have databases in the files
+/app/update.sh
 
 # run fastapi app
 exec uvicorn server.app.main:create_app --factory --host 0.0.0.0 --port 8000
