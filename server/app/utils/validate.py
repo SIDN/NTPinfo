f206fe8db44a5179a7747e26112e1cc8e2c508bc
Serban Orza <orza@student.tudelft.nl>
2025-06-19 12:22:03 +0200

    Added a sanitize method and fully tested it

--- server/app/utils/validate.py
+++ server/app/utils/validate.py
@@ -1,4 +1,5 @@
 import ipaddress
+import re
 from datetime import datetime, timezone
 from ipaddress import IPv4Address, IPv6Address
 from typing import Optional
@@ -84,3 +85,20 @@ def parse_ip(ip_str: str) -> IPv4Address | IPv6Address | None:
     except ValueError:
         # print("Invalid IP address")
         return None
+
+def sanitize_string(s: str) -> str:
+    """
+    This method sanitize a string. It removes null characters (\x00) or control characters (\x01-\x1F)
+    or the DEL character (\x7F) from a string. It removes them. We do this because our database does not
+    allow these characters (security concerns). It prints a warning if the string contains such characters.
+
+    Args:
+        s (str): The string to sanitize.
+
+    Returns:
+        str: The sanitized string.
+    """
+    good_string = re.sub(r'[\x00-\x1F\x7F]', '', s)
+    if good_string != s:
+        print(f"Warning, strange characters detected in {s}. Changed the word to {good_string}")
+    return good_string
