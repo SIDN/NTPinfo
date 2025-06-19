2740d6c01c6c683b447956ad2339185a2eca72dd
Horia Botezatu <h.a.botezatu@student.tudelft.nl>
2025-06-19 17:46:46 +0200

    Uncommented script method

--- update_geolite_and_bgptools_dbs.sh
+++ update_geolite_and_bgptools_dbs.sh
@@ -39,27 +39,27 @@ download_anycast_db() {
 }
 
 download_and_extract() {
-#  DB_NAME=$1
-#  echo "Downloading ${DB_NAME}..."
-#  curl -O -J -L -u "$ACCOUNT_ID:$LICENSE_KEY" "https://download.maxmind.com/geoip/databases/${DB_NAME}/download?suffix=tar.gz"
-#
-#  echo "Extracting ${DB_NAME}..."
-#  tar -xzf ${DB_NAME}_*.tar.gz
-#
-#  EXTRACTED_DIR=$(tar -tzf ${DB_NAME}_*.tar.gz | head -1 | cut -f1 -d"/")
-#  MMDB_FILE=$(find "$EXTRACTED_DIR" -name "*.mmdb" | head -n 1)
-#
-#  if [ -z "$MMDB_FILE" ]; then
-#    echo "Error: .mmdb file not found for ${DB_NAME}."
-#    return 1
-#  fi
-#
-#  echo "Moving .mmdb to ${TARGET_DIR}/${DB_NAME}.mmdb"
-#  mv "$MMDB_FILE" "${TARGET_DIR}/${DB_NAME}.mmdb"
-#
-#  echo "Cleaning up..."
-#  rm ${DB_NAME}_*.tar.gz
-#  rm -r "$EXTRACTED_DIR"
+  DB_NAME=$1
+  echo "Downloading ${DB_NAME}..."
+  curl -O -J -L -u "$ACCOUNT_ID:$LICENSE_KEY" "https://download.maxmind.com/geoip/databases/${DB_NAME}/download?suffix=tar.gz"
+
+  echo "Extracting ${DB_NAME}..."
+  tar -xzf ${DB_NAME}_*.tar.gz
+
+  EXTRACTED_DIR=$(tar -tzf ${DB_NAME}_*.tar.gz | head -1 | cut -f1 -d"/")
+  MMDB_FILE=$(find "$EXTRACTED_DIR" -name "*.mmdb" | head -n 1)
+
+  if [ -z "$MMDB_FILE" ]; then
+    echo "Error: .mmdb file not found for ${DB_NAME}."
+    return 1
+  fi
+
+  echo "Moving .mmdb to ${TARGET_DIR}/${DB_NAME}.mmdb"
+  mv "$MMDB_FILE" "${TARGET_DIR}/${DB_NAME}.mmdb"
+
+  echo "Cleaning up..."
+  rm ${DB_NAME}_*.tar.gz
+  rm -r "$EXTRACTED_DIR"
 
   echo "${DB_NAME}.mmdb update complete!"
 }
