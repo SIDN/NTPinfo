4e1b36bba800e5521e5399b57933990c1edffaca
George Andrei <g.andrei-1@student.tudelft.nl>
2025-06-19 12:20:19 +0200

    Added tests for calculateStatus

--- client/src/utils/calculateStatus.ts
+++ client/src/utils/calculateStatus.ts
@@ -2,7 +2,7 @@ import { NTPData, RIPEData } from "./types"
 
 export const calculateStatus = (ntpData: NTPData, ripeData: RIPEData): string => {
     if (Math.max(Math.abs(ntpData.offset), Math.abs(ripeData.measurementData.offset)) < 100)
-        return "PASS"
+        return "PASSING"
     else if (Math.min(Math.abs(ntpData.offset), Math.abs(ripeData.measurementData.offset)) < 100)
         return "CAUTION"
     return "FAILING"
