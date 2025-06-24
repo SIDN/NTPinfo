a9567ced9d224ad2f9f5fb92237a0882618bc730
Horia Botezatu <h.a.botezatu@student.tudelft.nl>
2025-06-24 13:04:46 +0200

    Added configurable ports for docker

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,22 +1,26 @@
-import { defineConfig, loadEnv } from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 import 'vitest/config'
 // https://vite.dev/config/
 export default defineConfig(({mode}) => {
-  
-  const env = loadEnv(mode, process.cwd())
-  return {
-    plugins: [react()],
-    test: {
-      environment: 'jsdom',
-      globals: true,
-      setupFiles: '/src/tests/setup_tests.ts',
-      coverage: {
-        include: ['src/**/*.{ts,tsx,js,jsx}']
-      }
-  },
-  server: {
-   host: env.VITE__CLIENT_HOST || 'localhost',
-    port: parseInt(env.VITE_CLIENT_PORT) || 5173
-  }
-}})
+
+    const env = loadEnv(mode, process.cwd())
+    return {
+        plugins: [react()],
+        build: {
+            sourcemap: false,
+        },
+        test: {
+            environment: 'jsdom',
+            globals: true,
+            setupFiles: '/src/tests/setup_tests.ts',
+            coverage: {
+                include: ['src/**/*.{ts,tsx,js,jsx}']
+            }
+        },
+        server: {
+            host: env.VITE_CLIENT_HOST || 'localhost',
+            port: parseInt(env.VITE_CLIENT_PORT) || 5173
+        }
+    }
+})
